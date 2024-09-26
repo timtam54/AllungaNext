56fd1d8c1def92cba2eccdc4dc7deb145925d18c
timhams <timtam54@hotmail.com>
2024-09-26 13:44:54 +1000

    fix header

--- components/header.tsx
+++ components/header.tsx
@@ -7,7 +7,8 @@ import GroupIcon from '@mui/icons-material/Group';
 import ExposureIcon from '@mui/icons-material/Exposure';
 import { msalInstance, handleLogout } from '@/msal/msal'
 import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
-import { ChevronDown, Search, LogOut, Users, Home, Database } from 'lucide-react'
+import { ChevronDown, Search, LogOut, Users, Home, Database, FileText } from 'lucide-react'
+//import { ChevronDown, LogOut, Home, Database, CloudSun } from 'lucide-react'
 import ScheduleActual from '@/components/ScheduleActual'
 import ViewHeadlineIcon from '@mui/icons-material/ViewHeadline';
 import Radiation from '@/components/radiation'
@@ -18,13 +19,21 @@ import { useState } from 'react';
 import { ChevronDownIcon } from '@heroicons/react/20/solid'
 export default function Component() {
   
-
+  const [radrptOpen, setRadrptOpen] = useState(false)
   const user = msalInstance.getActiveAccount()
   const [isOpen, setIsOpen] = useState(false)
 
   const toggleDropdown = () => setIsOpen(!isOpen)
+  /*
+  {weatherOpen && <Weather closeModal={() => setWeatherOpen(false)} />}
+      {chartSimpleOpen && <ChartSimple closeModal={() => setChartSimpleOpen(false)} />}
+      {schedrptOpen && <ScheduleActual Rpt={rpt} closeModal={() => setSchedrptOpen(false)} />}*/
   return (
     <header>
+
+
+      {radrptOpen && <Radiation closeModal={() => setRadrptOpen(false)} />}
+
       <div style={{display:'flex', justifyContent:'space-between',alignContent:'center'}}>
       <div style={{display:'flex',alignContent:'center'}}>
             <img src="/logo.png" alt="Logo"  />
@@ -49,28 +58,20 @@ export default function Component() {
         <div className="py-1">
           <MenuItem>
             <Link 
-             href={{
-              pathname:"/"}}  className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
-            >
-              <Search className="w-4 h-4 mr-2" />
+             href={"/"}  className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"><Search className="w-4 h-4 mr-2" />
               Search
             </Link>
           </MenuItem>
           
           <MenuItem>
-            <Link 
-              href="" onClick={() => handleLogout('redirect')}
-              className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
-            > <ExitToAppIcon className="w-4 h-4 mr-2" />
-                  Sign Out
+            <Link   href="" onClick={() => handleLogout('redirect')}
+              className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"  >
+            <ExitToAppIcon className="w-4 h-4 mr-2" />Sign Out
             </Link>
           </MenuItem>
-          
-
-          <MenuItem>
+           <MenuItem>
             <Link 
-              href={{
-                pathname:"/clientaccess"}}
+              href={"/clientaccess"}
               className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
             > <Users className="w-4 h-4 mr-2" />
                   Client Access
@@ -133,7 +134,38 @@ export default function Component() {
       </MenuItems>
     </Menu>
 
+    <Menu as="div" className="relative inline-block text-left">
+      <div>
+        <MenuButton className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+        <FileText className="w-4 h-4 mr-2" />Reports
+          <ChevronDownIcon aria-hidden="true" className="-mr-1 h-5 w-5 text-gray-400" />
+        </MenuButton>
+      </div>
 
+      <MenuItems
+        transition
+        className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
+      >
+        <div className="py-1">
+          <MenuItem>
+          <Link   href={"/rackrpt"}  className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
+            ><AddLocationIcon className="w-4 h-4 mr-2" />
+              Rack Report
+            </Link>
+          </MenuItem>
+          <MenuItem>
+          <Link href="" onClick={() => setRadrptOpen(true)} className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
+            ><WbSunnyIcon className="w-4 h-4 mr-2" />
+              Radiation
+            </Link>
+          </MenuItem>
+          
+        </div>
+
+        
+
+      </MenuItems>
+    </Menu>
     <nav>
           <div className="relative">
             <button
